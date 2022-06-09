1883c655417665f8da4839dceac1f74feb2a990e
danielemalitesta <d.malitesta@gmail.com>
2022-06-09 18:28:48 +0200

    First commit.

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,18 @@
-FROM nvidia/cuda:11.5.1-cudnn8-runtime-ubuntu20.04
+FROM nvidia/cuda:11.5.1-cudnn8-devel-ubuntu20.04
 
 ENV DEBIAN_FRONTEND=noninteractive
 
-RUN apt-get update -y
-
-RUN apt-get install zip -y && \
-    apt-get install unzip -y && \
-    apt-get install nano -y && \
-    apt-get install htop -y && \
-    apt-get install git -y && \
-    apt-get install curl -y
-
-RUN apt-get install software-properties-common -y && \
+RUN apt-get update -y && \
+    apt-get install -y unzip git curl software-properties-common && \
     add-apt-repository ppa:deadsnakes/ppa -y && \
-    apt-get install python3.8 -y && \
-    apt-get install python3.8-venv python3-venv -y && \
+    apt-get install -y python3.8 python3.8-distutils && \
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
-    python3.8 get-pip.py
-
-RUN git clone https://github.com/sisinflab/Graph-Demo-CIKM2022.git
-
-WORKDIR /Graph-Demp-CIKM2022
-
-RUN python3.8 -m venv venv && \
-    source venv/bin/activate && \
+    python3.8 get-pip.py && \
+    git clone https://github.com/sisinflab/Graph-Demo-CIKM2022.git && \
     pip install --upgrade pip && \
-    pip install -r requirements.txt
+    pip install -r Graph-Demo-CIKM2022/requirements.txt && \
+    pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.11.0+cu115.html
 
-RUN gdown 1HktsX2gBlox1QwOeDX39fKbNaMTKLg6j && \
-    unzip data.zip && \
-    rm data.zip
+WORKDIR Graph-Demo-CIKM2022
+
+RUN python3.8 start_experiment.py
--- start_experiments.py
+++ start_experiments.py
@@ -1,9 +1,83 @@
 from elliot.run import run_experiment
-import argparse
+import gdown
+import os
+import shutil
 
-parser = argparse.ArgumentParser(description="Run training and evaluation.")
-parser.add_argument('--model', type=str, default='ngcf')
-parser.add_argument('--dataset', type=str, default='movielens')
-args = parser.parse_args()
+print('MODELS:')
+print(u'''
++--------------------------------------------------------------------------------------------------------+----------+--------------------------------------------------------+
+|                                                 Paper                                                  |   Name   |                          Link                          |
++--------------------------------------------------------------------------------------------------------+----------+--------------------------------------------------------+
+| Neural Graph Collaborative Filtering                                                                   | ngcf     | https://dl.acm.org/doi/10.1145/3331184.3331267         |
+| LightGCN: Simplifying and Powering Graph Convolution Network for Recommendation                        | lightgcn | https://dl.acm.org/doi/10.1145/3397271.3401063         |
+| Disentangled Graph Collaborative Filtering                                                             | dgcf     | https://dl.acm.org/doi/abs/10.1145/3397271.3401137     |
+| Revisiting Graph Based Collaborative Filtering: A Linear Residual Graph Convolutional Network Approach | lr-gccf  | https://ojs.aaai.org//index.php/AAAI/article/view/5330 |
+| UltraGCN: Ultra Simplification of Graph Convolutional Networks for Recommendation                      | ultragcn | https://dl.acm.org/doi/10.1145/3459637.3482291         |
+| How Powerful is Graph Convolution for Recommendation?                                                  | gfcf     | https://dl.acm.org/doi/abs/10.1145/3459637.3482264     |
++--------------------------------------------------------------------------------------------------------+----------+--------------------------------------------------------+
+''')
 
-run_experiment(f"config_files/{args.model}/{args.dataset}.yml")
+while True:
+    model = str(input('Insert model name (ngcf, lightgcn, dgcf, lr-gccf, ultragcn, gfcf): '))
+    if model.lower() in ['ngcf', 'lightgcn', 'dgcf', 'lr-gccf', 'ultragcn', 'gfcf']:
+        break
+    else:
+        print('Sorry, the model should be one of these: ngcf, lightgcn, dgcf, lr-gccf, ultragcn, gfcf')
+
+print('\n\n')
+
+while True:
+    layer = str(input('Insert number of explored hops (0 for ultragcn and gfcf, 1, 2, 3, 4): '))
+    if layer.lower() in ['0', '1', '2', '3', '4']:
+        break
+    else:
+        print('Sorry, the number of explored hops should be one of these: 0, 1, 2, 3, 4')
+
+print('\n\n')
+print('DATASETS:')
+print(u'''
++----------------------+--------+--------+--------------+
+|       Dataset        | Users  | Items  | Interactions |
++----------------------+--------+--------+--------------+
+| Movielens-1M         |  5,915 |  2,753 |      570,622 |
+| Amazon Digital Music |  8,328 |  6,275 |       99,400 |
+| Epinions             | 14,341 | 13,145 |      269,170 |
++----------------------+--------+--------+--------------+
+''')
+
+while True:
+    dataset = str(input('Insert dataset name (movielens-1m, amazon digital music, epinions): '))
+    if dataset.lower() in ['movielens-1m', 'amazon digital music', 'epinions']:
+        if dataset.lower() == 'movielens-1m':
+            gdown.download_folder("https://drive.google.com/drive/folders/1ZIAFa63TAP76D5qSRaxltN0jBwO3aPbE",
+                                  quiet=True, use_cookies=False)
+            if not os.path.exists('data/movielens'):
+                os.makedirs('data/movielens')
+            files_list = os.listdir('movielens')
+            for files in files_list:
+                shutil.move(files, 'data/movielens/')
+            os.rmdir('movielens')
+        elif dataset.lower() == 'epinions':
+            gdown.download_folder("https://drive.google.com/drive/folders/1uj7X5PdaHTbES-YcoKnQMEuuiLAjC-_k",
+                                  quiet=True, use_cookies=False)
+            if not os.path.exists('data/epinions'):
+                os.makedirs('data/epinions')
+            files_list = os.listdir('epinions')
+            os.rmdir('epinions')
+            for files in files_list:
+                shutil.move(files, 'data/epinions/')
+        else:
+            if not os.path.exists('data/amazon_music'):
+                os.makedirs('data/amazon_music')
+            gdown.download_folder("https://drive.google.com/drive/folders/1ABWWOE9PONypZw1qV80VrRu2T7QQicRn",
+                                  quiet=True, use_cookies=False)
+            files_list = os.listdir('amazon_music')
+            for files in files_list:
+                shutil.move(files, 'data/amazon_music/')
+            os.rmdir('amazon_music')
+        break
+    else:
+        print('Sorry, the dataset should be one of these: movielens-1m, amazon digital music, epinions')
+
+print('\n\n')
+run_experiment(f"config_files/{model}/{dataset}/{layer}.yml")
